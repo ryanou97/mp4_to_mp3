17fd4a36a812b4e88b110a213bb35a1bef98a49f
ryanou97 <1997Ouasd_>
2023-10-28 20:17:12 +0800

    first commit

---
+++ mp4tomp3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os
+from moviepy.editor import *
+
+def MP4ToMP3(mp4, mp3):
+    FILETOCONVERT = AudioFileClip(mp4)
+    FILETOCONVERT.write_audiofile(mp3)
+    FILETOCONVERT.close()
+
+VIDEO_FILE_PATH = "C:/Course/If you're willing to imagine it, it's possible to make it happen!.mp4"
+AUDIO_FILE_PATH = "C:/Course/If you're willing to imagine it, it's possible to make it happen!.mp3"
+
+MP4ToMP3(VIDEO_FILE_PATH, AUDIO_FILE_PATH)
+# MoviePy - Done.                    
+
+
+# get now path
+print(os.getcwd())
