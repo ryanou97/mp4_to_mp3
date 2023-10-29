97311c67505119ffd31dd0d7d63ad3e4cf358be5
ryanou97 <zhonglinou.1997@gmail.com>
2023-10-29 12:56:16 +0800

    specified path and file

--- mp4tomp3.py
+++ mp4tomp3.py
@@ -5,16 +5,18 @@ Spyder Editor
 This is a temporary script file.
 """
 
+
 import os
-from moviepy.editor import *
+from moviepy.editor import *   # pip install moviepy
 
 def MP4ToMP3(mp4, mp3):
     FILETOCONVERT = AudioFileClip(mp4)
     FILETOCONVERT.write_audiofile(mp3)
     FILETOCONVERT.close()
 
-VIDEO_FILE_PATH = "C:/Course/If you're willing to imagine it, it's possible to make it happen!.mp4"
-AUDIO_FILE_PATH = "C:/Course/If you're willing to imagine it, it's possible to make it happen!.mp3"
+# VIDEO_FILE_PATH = "C:/Course/If you're willing to imagine it, it's possible to make it happen!.mp4"
+# AUDIO_FILE_PATH = "C:/Course/If you're willing to imagine it, it's possible to make it happen!.mp3"
+
 
 MP4ToMP3(VIDEO_FILE_PATH, AUDIO_FILE_PATH)
 # MoviePy - Done.                    
